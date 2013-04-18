b6f58fd90b97f147ff979c5887ceab6007299eba
Izhar Firdaus <izhar@inigo-tech.com>
2013-04-18 14:00:43 +0800

    added initial template for inigo_buildout and inigo_theme

--- inigo/templer/__init__.py
+++ inigo/templer/__init__.py
@@ -1,2 +1,4 @@
 # This package may contain traces of nuts
 from inigo.templer.inigo_plone import InigoPlone
+from inigo.templer.inigo_buildout import InigoBuildout
+from inigo.templer.inigo_theme import InigoTheme
